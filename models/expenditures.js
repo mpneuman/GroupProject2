bf3c26ce66940554ceaeda1e417f3ce7e4d2d1b6
Rori Jackson <rorijackson@wifi_guest-10-38-116-136.uncc.edu>
2018-04-02 19:15:57 -0400

    adding

--- models/expenditures.js
+++ models/expenditures.js
@@ -1,29 +1,29 @@
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
-
-var Expenditures = sequelize.define("expenditure", {
-  id: {
-    autoIncrement: true,
-    primaryKey: true,
-    type: Sequelize.INTEGER
-  },
-
-  payee: {
-    type: Sequelize.STRING,
-    notEmpty: true
-  },
-  amount_due: {
-    type: Sequelize.FLOAT
-  },
-  category: {
-    type: Sequelize.STRING
-  },
-  notes: {
-    type: Sequelize.STRING
-  }
-});
-
-Expenditures.sync();
-
-module.export = Expenditures;
+module.exports = function (sequelize, Sequelize) {
+    var Expenditures = sequelize.define("expenditure", {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+    
+      payee: {
+        type: Sequelize.STRING,
+        notEmpty: true
+      },
+      amount_paid: {
+        type: Sequelize.FLOAT,
+        acceptNull: false
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      notes: {
+        type: Sequelize.STRING
+      },
+      lastLogin: {
+          type: Sequelize.DATE
+      }
+    });
+    return Expenditures
+    
+    }
